ec2239ea3f9b528b1416407ea57822989fbf78fb
Jared Frank <jfrank1120@verizon.net>
2022-04-08 17:22:02 -0400

    Refactoring for distribution
    Moved files into src folder so they would be included in the build

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Open-Grade',
+    version='1.0.0',
+    packages=['src', 'src.util'],
+    url='https://github.com/jfrank1120/Open-Grade',
+    license='MIT',
+    author='jfrank1120',
+    author_email='jfrank1120@github.com',
+    description='Open source grading software for coding professors/teachers to use'
+)
